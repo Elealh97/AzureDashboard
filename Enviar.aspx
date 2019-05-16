0ec9cd5e5a1833fdd1cfd715169d21cf1775c152
Edgar Leal <elealh@miumg.edu.gt>
2019-05-16 16:16:09 -0600

    Agregar archivos de proyecto.

---
+++ Enviar.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Enviar.aspx.cs" Inherits="DashBoard.Enviar" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <script src="Scripts/jquery-3.0.0.min.js"></script>
+    <script src="Scripts/bootstrap.min.js"></script>
+    <link href="Content/bootstrap.min.css" rel="stylesheet" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:FileUpload CssClass="form-control" ID="FileUpload1" runat="server" />
+            <br />
+            <asp:Button CssClass="btn btn-info" ID="Button1" runat="server" Text="Enviar" OnClick="Button1_Click"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Enviar.aspx.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Threading;
+using Microsoft.ServiceBus.Messaging;
+using System.Text;
+using Newtonsoft.Json;
+using LinqToExcel;
+using OfficeOpenXml;
+using System.IO;
+using System.Data;
+
+namespace DashBoard
+{
+    public partial class Enviar : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected class Paciente
+            {
+            String Nombre;
+            String Departamento;
+            String FechaNac;
+            String EstadoNI;
+            String EstadoNA;
+
+            public string Nombre1 { get => Nombre; set => Nombre = value; }
+            public string Departamento1 { get => Departamento; set => Departamento = value; }
+            public string FechaNac1 { get => FechaNac; set => FechaNac = value; }
+            public string EstadoNI1 { get => EstadoNI; set => EstadoNI = value; }
+            public string EstadoNA1 { get => EstadoNA; set => EstadoNA = value; }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+
+            if (FileUpload1.HasFile && Path.GetExtension(FileUpload1.FileName) == ".xlsx")
+            {
+                using (var excel = new ExcelPackage(FileUpload1.PostedFile.InputStream))
+                {
+                    var tbl = new DataTable();
+                    var ws = excel.Workbook.Worksheets.First();
+                    var hasHeader = true;  // adjust accordingly
+                    foreach (var firstRowCell in ws.Cells[1, 1, 1, ws.Dimension.End.Column])
+                    {
+                        tbl.Columns.Add(hasHeader ? firstRowCell.Text : String.Format("Column {0}", firstRowCell.Start.Column));
+                    }
+
+                    int startRow = hasHeader ? 2 : 1;
+                    for (int rowNum = startRow; rowNum <= ws.Dimension.End.Row; rowNum++)
+                    {
+                        var wsRow = ws.Cells[rowNum, 1, rowNum, ws.Dimension.End.Column];
+                        DataRow row = tbl.NewRow();
+                        foreach (var cell in wsRow)
+                            row[cell.Start.Column - 1] = cell.Text;
+                        tbl.Rows.Add(row);
+                    }
+
+
+
+                    foreach (DataRow row in tbl.Rows)
+                    {
+                        Paciente paciente = new Paciente();
+                        paciente.Nombre1 = row[1].ToString();
+                        paciente.Departamento1 = row[2].ToString();
+                        paciente.EstadoNI1 = row[3].ToString();
+                        paciente.EstadoNA1 = row[4].ToString();
+
+                        var eventHubClient = EventHubClient.CreateFromConnectionString("Endpoint=sb://dashboardumg.servicebus.windows.net/;SharedAccessKeyName=CloudBasico;SharedAccessKey=GLr7AngnFNKOXCYAAmtZmdMcDBmlRwEb5cw7WSfdJwI=", "dashboardis");
+                        try
+                        {
+                            var message = JsonConvert.SerializeObject(paciente);
+                            eventHubClient.Send(new EventData(Encoding.UTF8.GetBytes(message)));
+                        }
+                        catch (Exception exception)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine( exception.Message);
+                            Console.ResetColor();
+                        }
+                    }
+                }
+            }
+
+
+
+        }
+
+
+
+        private List<Paciente> ExtraerExcel(string DireccionArchivo)
+        {
+            List<Paciente> listaEmpleados = new List<Paciente>();
+            var AbrirExcel = new ExcelQueryFactory(DireccionArchivo);
+            var ObtencionDatos = AbrirExcel.Worksheet("MARZO");
+
+            foreach (var elementos in ObtencionDatos)
+            {
+                var nuevoEmpleado = new Paciente
+                {
+                    Nombre1 = elementos.ElementAt(0).Value.ToString(),
+                    Departamento1 = elementos.ElementAt(1).Value.ToString(),
+                    EstadoNI1 = elementos.ElementAt(2).ToString(),
+                    EstadoNA1 = elementos.ElementAt(3).ToString(),
+                };
+
+                listaEmpleados.Add(nuevoEmpleado);
+            }
+
+            return listaEmpleados;
+        }
+    }
+}
